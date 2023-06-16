269a96b2636d749cb54d39269a76cab6c89625a1
Tatiana Novitskaya <passatik11@gmail.com>
2023-06-16 17:24:33 -0400

    Added HTML forecast with JS

--- js/index.js
+++ js/index.js
@@ -23,7 +23,6 @@ document.addEventListener("DOMContentLoaded", () => {
         buttonCurrentLocation.addEventListener("click", () => {
             getCurrentLocation()
         });
-
     }
 
     function setCity() {
@@ -63,7 +62,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     function getWeatherInfo(response) {
-        console.log(response)
         let feelsLikeCurrentTemp = Math.floor(response.data.main.feels_like);
         let windSpeed = Math.floor(response.data.wind.speed);
 
@@ -78,23 +76,19 @@ document.addEventListener("DOMContentLoaded", () => {
         let currentLongitude = position.coords.longitude;
         let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${currentLatitude}&lon=${currentLongitude}&appid=${apiKey}&units=metric`;
         getAxiosUrl(apiUrl);
+
+    }
+
+    function displayForecast(coordinates){
+        console.log(coordinates)
+        let apiKey = "0df6a9dd1987o3afdebba40233td58aa";
+        let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.lon}&lat=${coordinates.lat}&key=${apiKey}`;
+        axios.get(apiUrl).then(displayForecastHTML)
     }
 
     const myModal = new bootstrap.Modal(document.getElementById('myModal'), {
         keyboard: false
     })
-
-    function getAxiosUrl(apiUrl) {
-        axios.get(apiUrl).then((response) => {
-            getCurrentTemperature(response);
-            setCurrentCity(response);
-            changeBackground(response);
-            getWeatherInfo(response)
-        })
-            .catch(function (error) {
-                myModal.show()
-            })
-    }
 
 
     function setCurrentCity(response) {
@@ -128,17 +122,56 @@ document.addEventListener("DOMContentLoaded", () => {
         celsiusElement.classList.remove("active-convert");
         fahrenheitElement.classList.add("active-convert");
     }
-
     function changeToCelsius() {
-
         currentTempInCelsius.innerText = `${celsiusTemperature}°`;
         celsiusElement.classList.add("active-convert");
         fahrenheitElement.classList.remove("active-convert");
     }
 
+
+    function displayForecastHTML(response) {
+        console.log(response);
+
+        let forecastElement = document.querySelector(".forecast");
+        let forecastHTML = "";
+        let countForecastDays = 7;
+        for (let i = 1; i <= countForecastDays; i++) {
+            forecastHTML = forecastHTML + `<div class="col-1 forecast-item ">
+                        <div class="forecast-item-day">
+                            <p>Tue</p>
+                        </div>
+                        <div class="forecast-item-icon">
+                            <img src="images/clouds.png" alt="">
+                        </div>
+                        <div class="forecast-item-temp-max">
+                            <p>8 <span>&#176</span></p>
+                        </div>
+                        <div class="forecast-item-temp-min">
+                            <p>2 <span>&#176</span></p>
+                        </div>
+                    </div>`;
+            forecastElement.innerHTML = forecastHTML;
+        }
+
+    }
+
     let fahrenheitElement = document.querySelector(".fahrenheit");
     fahrenheitElement.addEventListener("click", changeToFahrengeit)
     let celsiusElement = document.querySelector(".celsius");
-    celsiusElement.addEventListener("click", changeToCelsius)
+    celsiusElement.addEventListener("click", changeToCelsius);
 
+
+    function getAxiosUrl(apiUrl) {
+        axios.get(apiUrl).then((response) => {
+            getCurrentTemperature(response);
+            setCurrentCity(response);
+            changeBackground(response);
+            getWeatherInfo(response);
+            displayForecastHTML(response);
+            displayForecast(response.data.coord)
+        })
+            .catch(function (error) {
+                myModal.show()
+            })
+    }
 })
